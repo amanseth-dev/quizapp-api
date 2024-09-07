993d7a6700e17e1f2d5e422c59682c9196694015
amanseth-dev <amanthecoder04@gmail.com>
2024-09-07 12:17:23 +0530

    Controller for getting all questions

--- src/main/java/com/example/quizapp/QuestionController.java
+++ src/main/java/com/example/quizapp/QuestionController.java
@@ -1,15 +1,20 @@
 package com.example.quizapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("question")
+@RequestMapping("/api/questions")
 public class QuestionController {
+    @Autowired
+    private QuestionService questionService;
     @GetMapping("allQuestions")
-    public String getAllQuestions() {
+    public List<Question> getAllQuestions() {
         // Implement logic to fetch all questions from the database
-        return "All questions";
+        return questionService.getAllQuestions();
     }
 }
