18612476058529787cef862df969efbb6322a875
Aman Seth <amanthecoder04@gmail.com>
2024-09-07 19:53:53 +0530

    repository now have a new method to find questions based on the category, because there is no default method found

--- src/main/java/com/example/quizapp/QuestionRepository.java
+++ src/main/java/com/example/quizapp/QuestionRepository.java
@@ -2,6 +2,9 @@ package com.example.quizapp;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
+    List<Question> findByCategory(String category);
 }
